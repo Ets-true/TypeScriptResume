df88f1802f3b00c0548b068756fe4fd16618ddbc
Pavel Tretyakov <pavel.tretyakov@sredasolutions.com>
2022-03-30 10:40:22 +0300

    Fix issue to start proxy server

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    process.env.REACT_APP_API_RSU_PREFIX,
+    process.env.REACT_APP_API_PREFIX,
     createProxyMiddleware({
       target: 'http://localhost:6006',
       changeOrigin: true,
