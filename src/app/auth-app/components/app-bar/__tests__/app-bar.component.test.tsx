6a03da74b5e68c5bc49628f56cbc2ad96df41859
Pavel Tretyakov <pavel.tretyakov@sredasolutions.com>
2022-04-15 19:24:29 +0300

    Included react-icons

--- src/app/auth-app/components/app-bar/__tests__/app-bar.component.test.tsx
+++ src/app/auth-app/components/app-bar/__tests__/app-bar.component.test.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 import { AppBar } from '../';
+import { StoreWrapper } from 'core/testing';
 
 describe('Component <AppBar />', () => {
   it('should be render', () => {
-    render(<AppBar />);
+    render(
+      <StoreWrapper>
+        <AppBar />
+      </StoreWrapper>
+    );
     const element = screen.getByTestId(/AppBar/i);
 
     expect(element).toBeInTheDocument();
--- src/app/auth-app/components/app-bar/app-bar.component.tsx
+++ src/app/auth-app/components/app-bar/app-bar.component.tsx
@@ -1,21 +1,40 @@
 import React from 'react';
 import tw, { styled } from 'twin.macro';
+import { useNavigate } from 'react-router-dom';
+
+import { CgProfile } from 'react-icons/cg';
+import { HomePage } from 'app/auth-app/home/home.page';
 
 import logoImage from './assets/logo.png';
 
 interface AppBarProps {}
 
 export function AppBarComponent(props: AppBarProps) {
+  const navigate = useNavigate();
+
   return (
     <Wrapper data-testid="AppBar">
       <LogoImage src={logoImage} alt="Логотип" />
-      <LogoText>Бурочки</LogoText>
+      <LogoText onClick={() => navigate(HomePage.route)}>Бурочки</LogoText>
+      <ActionsWrapper>
+        <Button>
+          <CgProfile />
+        </Button>
+      </ActionsWrapper>
     </Wrapper>
   );
 }
 
+const Button = styled.div`
+  ${tw`cursor-pointer`};
+`;
+
+const ActionsWrapper = styled.div`
+  ${tw`ml-auto flex`};
+`;
+
 const LogoText = styled.div`
-  ${tw`ml-3`};
+  ${tw`ml-3 cursor-pointer`};
   font-size: 18px;
   text-transform: uppercase;
 `;
