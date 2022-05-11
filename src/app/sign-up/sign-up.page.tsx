498cba9073c3a588c12e113de720ab2876a5aa5a
Pavel Tretyakov <pavel.tretyakov@sredasolutions.com>
2022-05-11 19:01:53 +0700

    Обновлена вёрстка входа и регистрации

--- src/app/sign-up/sign-up.page.tsx
+++ src/app/sign-up/sign-up.page.tsx
@@ -13,85 +13,93 @@ export function SignUpPage(props: SignUpProps) {
   const { handleSubmit, initialValues, schema } = useSignUpFormManager();
 
   return (
-    <StyledCard data-testid="SignUp">
-      <Card.Header>Регистрация</Card.Header>
+    <RowWrapper>
+      <Col lg="5">
+        <StyledCard data-testid="SignUp">
+          <Card.Header>Регистрация</Card.Header>
 
-      <Card.Body>
-        <FormWrapper>
-          <Formik
-            onSubmit={handleSubmit}
-            initialValues={initialValues}
-            validationSchema={schema}
-          >
-            {({ handleSubmit, values, errors, handleChange }) => (
-              <Form onSubmit={handleSubmit}>
-                <RowWrapper>
-                  <Col lg="5">
-                    <Form.Group controlId="validationFormikUsername">
-                      <Form.Label>Логин</Form.Label>
+          <Card.Body>
+            <FormWrapper>
+              <Formik
+                onSubmit={handleSubmit}
+                initialValues={initialValues}
+                validationSchema={schema}
+              >
+                {({ handleSubmit, values, errors, handleChange }) => (
+                  <Form onSubmit={handleSubmit}>
+                    <RowWrapper>
+                      <Col>
+                        <Form.Group controlId="validationFormikUsername">
+                          <Form.Label>Логин</Form.Label>
 
-                      <InputGroup hasValidation>
-                        <InputGroup.Text id="inputGroupPrepend">
-                          @
-                        </InputGroup.Text>
+                          <InputGroup hasValidation>
+                            <InputGroup.Text id="inputGroupPrepend">
+                              @
+                            </InputGroup.Text>
 
-                        <Form.Control
-                          type="text"
-                          placeholder="login"
-                          name="login"
-                          value={values.login}
-                          onChange={handleChange}
-                          isInvalid={!!errors.login}
-                        />
+                            <Form.Control
+                              type="text"
+                              placeholder="login"
+                              name="login"
+                              value={values.login}
+                              onChange={handleChange}
+                              isInvalid={!!errors.login}
+                            />
 
-                        <Form.Control.Feedback type="invalid">
-                          {errors.login}
-                        </Form.Control.Feedback>
-                      </InputGroup>
-                    </Form.Group>
-                  </Col>
-                </RowWrapper>
+                            <Form.Control.Feedback type="invalid">
+                              {errors.login}
+                            </Form.Control.Feedback>
+                          </InputGroup>
+                        </Form.Group>
+                      </Col>
+                    </RowWrapper>
 
-                <RowWrapper className="mt-3">
-                  <Col lg="5">
-                    <Form.Group controlId="validationCustom03">
-                      <Form.Label>Пароль</Form.Label>
+                    <RowWrapper className="mt-3">
+                      <Col>
+                        <Form.Group controlId="validationCustom03">
+                          <Form.Label>Пароль</Form.Label>
 
-                      <Form.Control
-                        name="password"
-                        type="password"
-                        placeholder="Password"
-                        value={values.password}
-                        onChange={handleChange}
-                        isInvalid={!!errors.password}
-                      />
+                          <Form.Control
+                            name="password"
+                            type="password"
+                            placeholder="Password"
+                            value={values.password}
+                            onChange={handleChange}
+                            isInvalid={!!errors.password}
+                          />
 
-                      <Form.Control.Feedback type="invalid">
-                        {errors.password}
-                      </Form.Control.Feedback>
-                    </Form.Group>
-                  </Col>
-                </RowWrapper>
+                          <Form.Control.Feedback type="invalid">
+                            {errors.password}
+                          </Form.Control.Feedback>
+                        </Form.Group>
+                      </Col>
+                    </RowWrapper>
 
-                <ActionsWrapper>
-                  <Col lg="2">
-                    <Button color="primary" type="submit">
-                      Зарегестрироваться
-                    </Button>
-                  </Col>
+                    <ActionsWrapper>
+                      <Col lg="2">
+                        <Button color="primary" type="submit">
+                          Зарегестрироваться
+                        </Button>
+                      </Col>
 
-                  <Col lg="3" className="text-right">
-                    <NavLink to={SignInRoute.route}>Войти</NavLink>
-                  </Col>
-                </ActionsWrapper>
-              </Form>
-            )}
-          </Formik>
-        </FormWrapper>
-      </Card.Body>
-    </StyledCard>
+                      <LinkCol>
+                        <NavLink to={SignInRoute.route}>Войти</NavLink>
+                      </LinkCol>
+                    </ActionsWrapper>
+                  </Form>
+                )}
+              </Formik>
+            </FormWrapper>
+          </Card.Body>
+        </StyledCard>
+      </Col>
+    </RowWrapper>
   );
 }
+
+const LinkCol = styled(Col)`
+  ${tw`lg:text-right md:text-right sm:text-right`};
+`;
 
 const StyledCard = styled(Card)`
   ${tw`mt-10`};
