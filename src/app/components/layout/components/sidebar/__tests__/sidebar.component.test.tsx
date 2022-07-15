c7af5f75717e33c8da0b4eb6a5cc01aaadbd3788
Pavel Tretyakov <pavel.tretyakov@sredasolutions.com>
2022-07-15 15:43:59 +1000

    Sidebar test wrapperd to store wrapper

--- src/app/components/layout/components/sidebar/__tests__/sidebar.component.test.tsx
+++ src/app/components/layout/components/sidebar/__tests__/sidebar.component.test.tsx
@@ -2,18 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 import { Sidebar } from '../';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { store } from 'core/state';
+import { StoreWrapper } from 'core/testing';
 
 describe('Component <Sidebar />', () => {
   it('should be render', () => {
     render(
-      <BrowserRouter>
-        <Provider store={store}>
-          <Sidebar />
-        </Provider>
-      </BrowserRouter>
+      <StoreWrapper>
+        <Sidebar />
+      </StoreWrapper>
     );
     const element = screen.getByTestId(/Sidebar/i);
 
